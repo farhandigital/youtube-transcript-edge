532a8929b549aa11f2e60a4db2959880eb220816
Invictus Navarchus <githubgpg.lumpiness807@passmail.com>
2026-03-08 16:56:07 +0700

    chore: add plain text with metadata test to manual script

--- scripts/manual-test.ts
+++ scripts/manual-test.ts
@@ -11,9 +11,25 @@ const transcript_srt = await fetchTranscript(video_id_or_url, {
 const transcript_vtt = await fetchTranscript(video_id_or_url, {
 	format: 'vtt',
 });
+const transcript_with_metadata = await fetchTranscript(video_id_or_url, {
+	format: 'json',
+	includeMetadata: true,
+});
 
+const transcript_plain_with_metadata = await fetchTranscript(video_id_or_url, {
+	format: 'text',
+	includeMetadata: true,
+});
 const folder = '../pipeline-debug';
 Bun.write(`${folder}/transcript.json`, JSON.stringify(transcript, null, 2));
 Bun.write(`${folder}/transcript.txt`, transcript_text);
 Bun.write(`${folder}/transcript.srt`, transcript_srt);
 Bun.write(`${folder}/transcript.vtt`, transcript_vtt);
+Bun.write(
+	`${folder}/transcript-with-metadata.json`,
+	JSON.stringify(transcript_with_metadata, null, 2),
+);
+Bun.write(
+	`${folder}/transcript-plain-with-metadata.txt`,
+	transcript_plain_with_metadata,
+);
